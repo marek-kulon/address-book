ce5c8764ab407ae01838d0b3c16d87ac5b52281a
Marek Kulon <marek.kulon@gmail.com>
2015-05-26 00:08:49 +0100

    person: string based version

---
+++ src/main/java/addressbook/person/Person.java
@@ -0,0 +1,26 @@
+package addressbook.person;
+
+public class Person {
+
+    private final String name;
+    private final String gender;
+    private final String dateOfBirth;
+
+    public Person(String name, String gender, String dateOfBirth) {
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+}
